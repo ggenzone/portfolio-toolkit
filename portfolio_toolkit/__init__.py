36d10214fde18a87a483d679c70ec9ca1bf90a4b
Guido Genzone <ggenzone@protonmail.com>
2025-07-25 12:32:24 +0200

    Bump version to 0.1.1

--- portfolio_toolkit/__init__.py
+++ portfolio_toolkit/__init__.py
@@ -1,6 +1,6 @@
 """Portfolio Toolkit - A comprehensive toolkit for portfolio analysis and management."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Guido Genzone"
 
 # Main imports for easy access
